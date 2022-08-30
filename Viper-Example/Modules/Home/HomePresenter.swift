dd15c83811d50dfcb2a06628b34f8680f514e84c
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-08-30 13:33:32 +0300

    adapted ViewController to Viper

--- Viper-Example/Modules/Home/HomePresenter.swift
+++ Viper-Example/Modules/Home/HomePresenter.swift
@@ -7,9 +7,13 @@
 
 import Foundation
 
-protocol HomePresenterInterface: AnyObject { }
+protocol HomePresenterInterface: AnyObject {
+    func notifyViewDidLoad()
+    var getItemCount: Int { get }
+    var getSectionCount: Int { get }
+}
 
-final class HomePresenter: HomePresenterInterface {
+final class HomePresenter {
     
     private weak var view: HomeViewInterface?
     private weak var router: HomeRouterInterface?
@@ -19,6 +23,23 @@ final class HomePresenter: HomePresenterInterface {
         self.view = view
         self.router = router
         self.interactor = interactor
+    }
+}
+
+// MARK: - Interface Setup
+extension HomePresenter: HomePresenterInterface {
+    
+    var getItemCount: Int {
+        10
+    }
+    
+    var getSectionCount: Int {
+        1
+    }
+    
+    func notifyViewDidLoad() {
+        view?.setupView()
+        view?.setTitle(with: "Home")
     }
 }
 
--- Viper-Example/Modules/Home/HomeViewController.swift
+++ Viper-Example/Modules/Home/HomeViewController.swift
@@ -7,14 +7,67 @@
 
 import UIKit
 
-protocol HomeViewInterface: AnyObject { }
+protocol HomeViewInterface: AnyObject {
+    func setupView()
+    func setTitle(with title: String)
+}
 
-class HomeViewController: UIViewController, HomeViewInterface {
+class HomeViewController: UIViewController {
 
     @IBOutlet private weak var collectionView: UICollectionView!
+    private var listConfig = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
     var presenter: HomePresenterInterface?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        presenter?.notifyViewDidLoad()
+    }
+}
+
+// MARK: - Interface Setup
+extension HomeViewController: HomeViewInterface, UICollectionViewDelegate {
+    
+    func setupView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.setCollectionViewLayout(createLayout(), animated: true)
+    }
+    
+    func setTitle(with title: String) {
+        self.title = title
+    }
+}
+
+// MARK: - Layout Configure
+extension HomeViewController {
+    
+    func createLayout() -> UICollectionViewCompositionalLayout {
+        let layout = UICollectionViewCompositionalLayout { index, environment in
+            var configuration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+            configuration.backgroundColor = .secondarySystemBackground
+            configuration.headerMode = .none
+            return .list(using: configuration, layoutEnvironment: environment)
+        }
+        return layout
+    }
+}
+
+// MARK: - CollectionView DataSource
+extension HomeViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        presenter?.getItemCount ?? 0
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        presenter?.getSectionCount ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "baseCell", for: indexPath) as? BaseCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
     }
 }
