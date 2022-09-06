a9cad747593f581afef92fb4fe6b0c2afe498ec9
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-09-06 23:34:35 +0300

    add dummy model datas

--- Viper-Example/Models/BaseModel.swift
+++ Viper-Example/Models/BaseModel.swift
@@ -8,4 +8,11 @@
 struct BaseModel: Codable {
     var title: String
     var description: String
+    
+    static let exampleDummyModels = [
+        BaseModel(title: "Dummy1", description: "Dummy1"),
+        BaseModel(title: "Dummy2", description: "Dummy1"),
+        BaseModel(title: "Dummy3", description: "Dummy1"),
+        BaseModel(title: "Dummy4", description: "Dummy1")
+    ]
 }
